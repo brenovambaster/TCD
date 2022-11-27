279020cb6fc339dc883eaf36b2cd4a014a1e89ff
breno <brenovambaster5@gmail.com>
2022-11-27 18:14:34 -0300

    Finalizado os testes de CRUD do pacote raca

--- AdotaPet/src/main/java/com/mycompany/adotapet/raca/Raca.java
+++ AdotaPet/src/main/java/com/mycompany/adotapet/raca/Raca.java
@@ -15,7 +15,6 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
-
 package com.mycompany.adotapet.raca;
 
 import com.mycompany.adotapet.entidade.Entidade;
@@ -23,14 +22,15 @@ import com.mycompany.adotapet.especie.Especie;
 
 /**
  * Classe Raca
+ *
  * @author Pedro Dias
  */
-public class Raca extends Entidade{
+public class Raca extends Entidade {
+
     private String nome;
     private Especie especie;
-    
-    //<editor-fold defaultstate="collapsed" desc="constructor">
 
+    //<editor-fold defaultstate="collapsed" desc="constructor">
     public Raca() {
     }
 
@@ -38,19 +38,17 @@ public class Raca extends Entidade{
         this.nome = nome.length() > 35 ? nome.substring(0, 35) : nome;
         this.especie = especie;
     }
-    
-    //</editor-fold>
-    
-    //<editor-fold defaultstate="collapsed" desc="getters/setters">
 
+    //</editor-fold>
+    //<editor-fold defaultstate="collapsed" desc="getters/setters">
     public String getNome() {
         return nome;
     }
 
     public void setNome(String nome) throws Exception {
-        if (nome == null || nome.length() > 35){
-            throw new Exception ("Nome não pode ter mais que 35 caracteres ou vazio!");
-        }else{
+        if (nome == null || nome.length() > 35) {
+            throw new Exception("Nome não pode ter mais que 35 caracteres ou vazio!");
+        } else {
             this.nome = nome;
         }
     }
@@ -66,12 +64,11 @@ public class Raca extends Entidade{
 
     @Override
     public String toString() {
-        return "Raca{" 
-                + "nome=" + nome 
-                + ", especie=" + especie 
+        return "Raca{"
+                + "nome=" + nome
+                + ", especie=" + especie
                 + ", " + super.toString()
                 + '}';
     }
-    
-    
+
 }
