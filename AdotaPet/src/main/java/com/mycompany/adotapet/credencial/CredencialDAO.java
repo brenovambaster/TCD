c92268c0e5c56e5256b7d8eabec41b2a203bb175
breno <brenovambaster5@gmail.com>
2022-12-05 20:33:34 -0300

    criando classse DAO para credencial

---
+++ AdotaPet/src/main/java/com/mycompany/adotapet/credencial/CredencialDAO.java
@@ -0,0 +1,122 @@
+/*
+ * Copyright (C) 2022 Breno Vambaster C. L
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package com.mycompany.adotapet.credencial;
+
+import com.mycompany.adotapet.repositorio.DAO;
+import com.mycompany.adotapet.usuario.Usuario;
+import com.mycompany.adotapet.tutor.Tutor;
+import com.mycompany.adotapet.tutor.TutorDAO;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Classe CredencialDAO
+ *
+ * @author Breno Vambaster C. L
+ */
+/*
+CREATE TABLE `credencial` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `email` varchar(40) NOT NULL,
+  `senha` varchar(40) NOT NULL,
+  `ativo` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
+ */
+public class CredencialDAO extends DAO<Credencial> {
+
+    public static final String TABLE = "credencial";
+
+    @Override
+
+    public String getSaveStatment() {
+        return "INSERT INTO " + TABLE + " (email, senha, usuario_id) values (?, MD5(?), ? ) ";
+    }
+
+    @Override
+    public String getUpdateStatment() {
+        return "UPDATE  " + TABLE + "  email=?, senha=MD5(?), usuario_id=? WHERE id=? ";
+    }
+
+    @Override
+    public void composeSaveOrUpdateStatement(PreparedStatement pstmt, Credencial e) {
+
+        try {
+            pstmt.setString(1, e.getEmail());
+            pstmt.setString(2, e.getSenha());
+            pstmt.setLong(3, e.getUsuario().getId());
+
+            if (e.getId() != null) {
+                pstmt.setLong(4, e.getId());
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(CredencialDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    @Override
+    public String getFindByIdStatment() {
+        return "SELECT * FROM " + TABLE + " WHERE id = ? ";
+    }
+
+    @Override
+    public String getFindAllStatment() {
+        return "SELECT * FROM " + TABLE;
+    }
+
+    @Override
+    public String getFindAllOnTrashStatement() {
+        return "SELECT * FROM " + TABLE + " WHERE ativo=false";
+    }
+
+    @Override
+    public String getMoveToTrashStatement() {
+        return "UPDATE  " + TABLE + " SET ativo=true WHERE id=?";
+    }
+
+    @Override
+    public String getRestoreFromTrashStatement() {
+        return "UPDATE  " + TABLE + " SET ativo=true WHERE id=?";
+    }
+
+    @Override
+    public Credencial extractObject(ResultSet resultSet) {
+        Credencial cred = new Credencial();
+        try {
+            cred.setId(resultSet.getLong("id"));
+            cred.setEmail(resultSet.getString("email"));
+            cred.setSenha(resultSet.getString("senha"));
+            cred.setAtivo(resultSet.getBoolean("ativo"));
+            cred.setUsuario(new TutorDAO().findById(resultSet.getLong("usuario_id")));
+        } catch (SQLException ex) {
+            Logger.getLogger(CredencialDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (Exception ex) {
+            Logger.getLogger(CredencialDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return cred;
+    }
+
+}
